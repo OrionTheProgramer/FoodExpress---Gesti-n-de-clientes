05dc155183b0e440b0f1cd91015a5e475e373dfd
OrionTheProgramer <167390483+OrionTheProgramer@users.noreply.github.com>
2025-05-29 23:16:35 -0400

    Docs: Actualizado

--- src/main/java/com/foodexpress/microservice/Config/SecurityConfig.java
+++ src/main/java/com/foodexpress/microservice/Config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SecurityConfig {
 
+    // Inicializar el password
     @Bean
     public Password passwordEncoder() {
         return new Password();
